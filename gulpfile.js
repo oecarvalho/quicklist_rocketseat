0aff7035ada5db567ac4c428b21a6cf017079f7d
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2025-01-04 00:55:16 -0300

    configurando gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss'); // Para usar PostCSS
 const autoprefixer = require('autoprefixer'); // Autoprefixer correto para PostCSS
+const browserSync = require('browser-sync').create()
 
 const compilaSass = () => {
     return gulp.src('scss/*.scss')
@@ -10,7 +11,29 @@ const compilaSass = () => {
             overrideBrowserslist: ['last 2 versions', '> 1%', 'IE 11']
         })
     ])) // Autoprefixer para PostCSS
-        .pipe(gulp.dest('css/'));
+        .pipe(gulp.dest('css/'))
+        .pipe(browserSync.stream())
 };
 
 gulp.task('default', compilaSass);
+
+
+const browser = () => {
+    browserSync.init({
+        server: {
+            baseDir: './'
+        }
+    })
+}
+
+gulp.task('browser-sync', browser);
+
+
+const watch = () => {
+    gulp.watch('scss/*.scss', compilaSass);
+    gulp.watch('*.html').on('change', browserSync.reload)
+}
+
+gulp.task('watch', watch);
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
