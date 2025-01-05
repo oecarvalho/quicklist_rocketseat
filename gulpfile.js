b07234883bff6e5dfce527402ac05a00f10d31a3
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2025-01-04 22:45:56 -0300

    finalizando automação com o gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss'); // Para usar PostCSS
 const autoprefixer = require('autoprefixer'); // Autoprefixer correto para PostCSS
-const browserSync = require('browser-sync').create()
+const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+
 
 const compilaSass = () => {
     return gulp.src('scss/*.scss')
@@ -14,9 +18,37 @@ const compilaSass = () => {
         .pipe(gulp.dest('css/'))
         .pipe(browserSync.stream())
 };
+gulp.task('sass', compilaSass);
 
-gulp.task('default', compilaSass);
 
+const pluginsCSS = () => {
+    return gulp.src('css/lib/*.css')
+    .pipe(concat('plugins.css'))
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream())
+}
+gulp.task('pluginscss', pluginsCSS)
+
+const gulpJs = () => {
+    return gulp.src('js/scripts/*.js')
+    .pipe(concat('index.js'))
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('js/'))
+    .pipe(browserSync.stream())
+}
+gulp.task('alljs', gulpJs)
+
+const pluginsJs = () => {
+    return gulp
+    .src(['./js/lib/aos.min.js', './js/lib/swiper.min.js'])
+    .pipe(concat('plugins.js'))
+    .pipe(gulp.dest('js/'))
+    .pipe(browserSync.stream())
+}
+gulp.task('pluginsjs', pluginsJs)
 
 const browser = () => {
     browserSync.init({
@@ -25,15 +57,15 @@ const browser = () => {
         }
     })
 }
-
 gulp.task('browser-sync', browser);
-
 
 const watch = () => {
     gulp.watch('scss/*.scss', compilaSass);
+    gulp.watch('css/lib/*.css', pluginsCSS)
     gulp.watch('*.html').on('change', browserSync.reload)
+    gulp.watch('js/scripts/*', gulpJs)
+    gulp.watch('js/lib/*.js', pluginsJs)
 }
-
 gulp.task('watch', watch);
 
-gulp.task('default', gulp.parallel('watch', 'browser-sync'));
+gulp.task('default', gulp.parallel('watch', 'browser-sync', 'pluginscss', 'sass', 'alljs', 'pluginsjs'));
---
+++ js/index.js
@@ -0,0 +1,1 @@
+var number=32;console.log("buttons teste"),console.log("modal");
