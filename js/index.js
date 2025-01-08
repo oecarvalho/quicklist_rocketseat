7439bb1607b93487b70eb734ce2abdf8c79fd147
Felipe Carvalho <felipe.carvalho_2016@outlook.com>
2025-01-08 17:40:21 -0300

    finalizando dropdown

--- js/index.js
+++ js/index.js
@@ -54,3 +54,31 @@ btnFecharModal.addEventListener('click', () =>{
     let tagHtml = document.documentElement;
     tagHtml.classList.remove('show-modal');
 })
+
+
+const btnMenu = document.querySelectorAll('.js-btn-menu');
+const menuSite = document.querySelectorAll('.js-menu');
+
+btnMenu.forEach((btn, index)=>{
+    btn.addEventListener('click', (event)=>{
+        event.preventDefault();
+
+        menuSite.forEach(itemMenu => {
+            itemMenu.classList.remove('active');
+            itemMenu.addEventListener('mouseleave', () => {
+                itemMenu.classList.remove('active');
+
+                btnMenu.forEach(itemBtn => {
+                    itemBtn.classList.remove("active")
+                })
+            })
+        });
+
+        btnMenu.forEach(itemBtn => {
+            itemBtn.classList.remove("active")
+        })
+
+        btn.classList.add('active')
+        menuSite[index].classList.add('active');
+    })
+})
